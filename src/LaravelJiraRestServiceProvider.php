bbe0c26b18e93da83937090d1c08f4be2579ec71
Florian Ratz <florian.ratz@supsign.ch>
2020-11-16 11:51:42 +0100

    no message

---
+++ src/LaravelJiraRestServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Supsign\LaravelJiraRest;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelJiraRestServiceProvider extends ServiceProvider
+{
+
+    public function boot() 
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
